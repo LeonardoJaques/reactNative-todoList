671eac3265b4c2b1e90e63cf027b942826f8e8cc
leonardo jaques <jaques.projetos@outlook.com>
2021-06-23 09:30:56 -0300

    Changed all content to TabOneScreen

--- App.tsx
+++ App.tsx
@@ -1,67 +1,8 @@
-import React, { useState } from 'react';
-import { FlatList, KeyboardAvoidingView, Platform, StyleSheet, TextInput } from 'react-native';
-import TodoItem from './src/components/TodoItem';
+import React from 'react'
+import TabOneScreen from './src/components/screens/TabOneScreen'
 
-export default function App() {
-
-
-  const [title, setTitle] = useState("")
-
-  
-  const [todos, setTodos] = useState([
-    { id: 1, content: "Buy milk", isCompleted: false },
-    { id: 2, content: "Buy bear", isCompleted: false },
-    { id: 3, content: "Buy meat", isCompleted: false },
-    { id: 4, content: "Buy water", isCompleted: false },
-  ])
-
-
-  const creatNewItem = (atIndex: number) => {
-    const newTodos = [...todos]
-    newTodos.splice(atIndex, 0, {
-      id: todos.length + 1,
-      content: '',
-      isCompleted: false
-    })
-    setTodos(newTodos)
-  }
-
-  return (
-    <KeyboardAvoidingView style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === "ios" ? 200 : 0}
-    >
-      <TextInput style={styles.title}
-        value={title}
-        onChangeText={setTitle}
-        placeholder={'Title'}
-      />
-      <FlatList
-        data={todos}
-        renderItem={({ item, index }) => (<TodoItem todo={item} onSubmit={() => creatNewItem(index + 1)} />)}
-        style={{ width: "100%" }}
-      />
-
-    </KeyboardAvoidingView>
-  );
-
+const App = () => {
+  return <TabOneScreen />
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    padding: 12,
-  },
-  title: {
-    fontSize: 20,
-    color: 'white',
-    fontWeight: 'bold',
-    backgroundColor: "red",
-    borderRadius: 10,
-    width: "100%",
-    textAlign: "center",
-    marginBottom: 12
-  },
 
-})
+export default App
